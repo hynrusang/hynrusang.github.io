3ab6f2039916708ed2902d3537aff3f8eac018cc
hynrusang <mashmalo1871@gmail.com>
2021-10-20 11:45:56 +0900

    v-2

---
+++ Effect/Script/main.js
@@ -0,0 +1,6 @@
+﻿function equl(e) {
+    var URL = document.getElementById("check").value + ".html";
+    if (e.key == "Enter") {
+        window.open(URL, "viewer", "width=650, height=950, location=0, manubar=0, status=0, toolbar=0")
+    }
+}
