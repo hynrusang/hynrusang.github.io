e2ae71d1fff96a7c9183bde5568d44f92451372a
hynrusang <mashmalo1871@gmail.com>
2023-08-09 06:05:16 +0900

    Update fragment.js

--- resource/js/fragment.js
+++ resource/js/fragment.js
@@ -29,7 +29,7 @@ const currentFragment = new LiveDataManager({
         type: String,
         observer: function () {
             subFragment[currentFragment.value("main")][this.value].launch();
-            autoReload()
+            autoReload();
         }
     })
 });
