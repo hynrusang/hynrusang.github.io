b63721f7ff1fb9b79f418d7746c34cb7a56acc11
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-10-29 18:56:27 +0900

    Update builder.js

--- resource/js/builder.js
+++ resource/js/builder.js
@@ -296,6 +296,7 @@ firebase.auth().onAuthStateChanged(async user => {
                 .catch(e => null);
             menuFragment.main.launch();
             mainFragment.main.launch();
+            scan("[rid=menu]").setAttribute("open", "");
             scan("!footer div input").forEach(obj => obj.onclick = e => current.value("tab", e.target.attributes.target.value));
         } else firebase.auth().signOut();
     }
