157458fbd5ef2f313706c384e51c625e7db3f859
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-11-06 23:12:53 +0900

    Update builder.js

--- resource/js/builder.js
+++ resource/js/builder.js
@@ -173,13 +173,13 @@ firebase.auth().onAuthStateChanged(async user => {
                             $("input", {
                                 type: "button",
                                 style: "background-image: url(resource/img/icon/del.png)",
-                                onclick: async e => {
+                                onclick: () => {
                                     if (confirm("정말 해당 채팅방에서 나가시겠습니까?\n데이터는 자동으로 삭제되지 않으며,\n추후 다시 들어올 시 신청을 다시 해야합니다.")) {
-                                        firebase.firestore().collection("chat").doc(scan(`[idx=a${index}] input`).attributes.target.value).get().then(data => {
+                                        firebase.firestore().collection("chat").doc(scan(`[idx=a${index}] input`).attributes.target.value).get().then(async data => {
                                             const owner = data.data().owner;
                                             if (owner == firebase.auth().currentUser.uid) alert("채팅방 관리자는 채팅방에서 나갈 수 없습니다.\n채팅방 메뉴에서 채팅방 삭제를 해야 합니다.");
                                             else {
-                                                data.ref.collection("enroll").doc(firebase.auth().currentUser.uid).delete();
+                                                await data.ref.collection("enroll").doc(firebase.auth().currentUser.uid).delete();
                                                 template.chatroom.splice(index, 1);
                                                 DB.value("chatroom", template.chatroom);
                                                 notifyDataChange();
