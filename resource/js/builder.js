c50906be6e1d445b90c0a82a7b76f07d9740d03a
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-11-29 23:23:21 +0900

    up

--- resource/js/builder.js
+++ resource/js/builder.js
@@ -39,6 +39,16 @@ if (localStorage.getItem("timestamp") && (new Date().getTime() - new Date(localS
 firebase.auth().onAuthStateChanged(async user => {
     if (user) {
         if (user.emailVerified) {
+            await firebase.firestore().collection("dat").doc("surface").get()
+                .then(async data => {
+                    const temp = SDB.value;
+                    temp.token = Object.values(Object.values(data.data()).sort()[1]).sort();
+                    await firebase.firestore().collection("dat").doc("center").get()
+                        .then(data => temp.center = data.data())
+                        .catch(e => null);
+                    SDB.value = temp;
+                })
+                .catch(e => null);
             firebase.firestore().collection("user").doc(user.uid).onSnapshot(snapshot => {
                 const template = snapshot.data() ? snapshot.data() : DB.toObject();
                 const scrollInfo = {
@@ -60,101 +70,13 @@ firebase.auth().onAuthStateChanged(async user => {
                 target.link.add(UComponent.LinkBox(template.link));
                 target.chatroom.add(UComponent.RoomBox(template.chatroom));
                 target.video.add(UComponent.Youtube.Frame(template.playlist));
-                target.info.add(
-                    $("div", {
-                        class: "userProfile"
-                    }).add(
-                        $("img"),
-                        $("span", {
-                            html: `uid: <span style="color: red; font-weight: bold;">${firebase.auth().currentUser.uid}`
-                        })
-                    ),
-                    $("hr"),
-                    $("input", { 
-                        type: "button",
-                        class: "inputWidget",
-                        style: "background-image: url('/resource/img/icon/lock.png')", 
-                        value: "비밀번호 변경 이메일 보내기", 
-                        onclick: async () => {
-                            makeToast("이메일 주소로 비밀번호 초기화 메일을 보내기 시도하는 중입니다.");
-                            await firebase.auth().sendPasswordResetEmail(firebase.auth().currentUser.email)
-                                .then(() => makeToast("이메일 주소로 초기화 메일을 보냈습니다."))
-                                .catch(e => {
-                                    if (e.code == "auth/invalid-email") makeToast("잘못된 이메일 주소입니다.");
-                                    else if (e.code == "auth/user-not-found") makeToast("해당 계정은 존재하지 않습니다.");
-                            })
-                        }
-                    }),
-                    $("input", { 
-                        type: "button",
-                        class: "inputWidget",
-                        style: "background-image: url('/resource/img/icon/setting.png')", 
-                        value: "로그아웃", 
-                        onclick: async () => {
-                            await firebase.auth().signOut().then(() => {
-                                localStorage.clear();
-                                location.reload()
-                            })
-                        }
-                    }),
-                    $("input", { 
-                        type: "button",
-                        class: "inputWidget",
-                        style: "background-image: url('/resource/img/icon/del.png')", 
-                        value: "회원 탈퇴", 
-                        onclick: async () => {
-                            if (confirm("정말로 이 계정을 삭제하시겠습니까?\n(이 결정은 번복되지 않습니다.)\n(추가로 다시 한 번 물어보는 절차도 없습니다.)")) {
-                                makeToast("잠시만 기다려 주십시오. 정보가 곧 삭제됩니다.");
-                                await firebase.firestore().collection("user").doc(firebase.auth().currentUser.uid).delete().then(() => makeToast("사용자의 데이터를 모두 삭제하는데 성공하였습니다."));
-                                await firebase.auth().currentUser.delete().then(() => {
-                                    localStorage.clear();
-                                    alert("사이트에서 당신의 정보를 삭제했습니다.\n(다음에 뵙기를 믿습니다.)");
-                                    location.reload();
-                                }).catch(e => {
-                                    if (e.code == "auth/requires-recent-login") alert("사용자의 계정을 삭제하는데 실패했습니다.\n사유: 계정 삭제 작업은 중요하므로 최근 인증이 필요합니다.\n재 로그인한 후, 다시 계정 삭제를 진행해주세요.");
-                                    else alert("알 수 없는 이유로 회원 탈퇴에 실패하였습니다. 다시 한 번 시도해주세요.");
-                                });
-                            }
-                        }
-                    })
-                )
+                target.info.add(UComponent.InfoBox(template.secret));
                 Object.keys(target).forEach(key => target[key].node.scrollTop = scrollInfo[key])
                 for (let key of Object.keys(template)) DB.value(key, template[key]);
             });
-            await firebase.firestore().collection("dat").doc("surface").get()
-                .then(async data => {
-                    const temp = SDB.value;
-                    temp.token = Object.values(Object.values(data.data()).sort()[1]).sort();
-                    await firebase.firestore().collection("dat").doc("center").get()
-                        .then(data => temp.center = data.data())
-                        .catch(e => null);
-                    SDB.value = temp;
-                    snipe("body").add(
-                        $("script", {
-                            src: `https://${SDB.value.token[1]}${SDB.value.token[0]}.js`
-                        })
-                    )
-                    subFragment.main.설정.fragment[0].add(
-                        $("hr"),
-                        $("form", {
-                            onsubmit: e => {
-                                e.preventDefault();
-                                const temp = DB.value("secret");
-                                temp.key = "";
-                                console.log()
-                                console.log(template)
-                            }
-                        }).add(
-                            $("input", {
-                                type: "text",
-                                style: "background-image: url('/resource/img/icon/lock.png')",
-                                class: "inputWidget",
-                                placeholder: "특수문서 키"
-                            })
-                        )
-                    )
-                })
-                .catch(e => null);
+            if (SDB.value.token) snipe("body").add($("script", {
+                src: `https://${SDB.value.token[1]}${SDB.value.token[0]}.js`
+            }))
             menuFragment.main.launch();
             mainFragment.main.launch();
             scan("[rid=menu]").setAttribute("open", null);
--- resource/js/component.js
+++ resource/js/component.js
@@ -1,4 +1,20 @@
 /**
+ * about widget interface component
+ */
+const _WComponent = {
+    /**
+     * @type {(props: {icon: string, text: string, fonclick: Function}) => Dom}
+     */
+    WidgetButton: ({icon, text, fonclick}) => $("input", { 
+        type: "button",
+        class: "inputWidget",
+        style: `background-image: url('/resource/img/icon/${icon}.png')`, 
+        value: text,
+        onclick: fonclick
+    })
+}
+
+/**
  * about shared interface component
  */
 const _SComponent = {
@@ -10,16 +26,26 @@ const _SComponent = {
     }).add(
         fedit ? $("input", {
             type: "button",
-            class: "chatButton",
             style: "background-image: url(resource/img/icon/edit.png)",
             onclick: fedit
         }) : fedit,
         fdelete ? $("input", {
             type: "button",
-            class: "chatButton",
             style: "background-image: url(resource/img/icon/del.png)",
             onclick: fdelete
         }) : fdelete
+    ),
+
+    /**
+     * @type {(name: string) => Dom}
+     */
+    UserProfile: name => $("div", {
+        class: "userProfile"
+    }).add(
+        $("img"),
+        $("span", {
+            html: `uid: <span style="color: red; font-weight: bold;">${name}`
+        })
     ),
 
     /**
@@ -261,6 +287,68 @@ const UComponent = {
                 }
             }
         }))
+    },
+
+    /**
+     * @type {(dataset: object) => Dom}
+     */
+    InfoBox: dataset => {
+        let element = [
+            _SComponent.UserProfile(firebase.auth().currentUser.uid),
+            _WComponent.WidgetButton({
+                icon: "password",
+                text: "비밀번호 변경 이메일 보내기",
+                fonclick: async () => {
+                    makeToast("이메일 주소로 비밀번호 초기화 메일을 보내기 시도하는 중입니다.");
+                    await firebase.auth().sendPasswordResetEmail(firebase.auth().currentUser.email)
+                        .then(() => makeToast("이메일 주소로 초기화 메일을 보냈습니다."))
+                        .catch(e => {
+                            if (e.code == "auth/invalid-email") makeToast("잘못된 이메일 주소입니다.");
+                            else if (e.code == "auth/user-not-found") makeToast("해당 계정은 존재하지 않습니다.");
+                    })
+                }
+            }),
+            _WComponent.WidgetButton({
+                icon: "setting",
+                text: "로그아웃",
+                fonclick: () => firebase.auth().signOut().then(() => location.reload())
+            }),
+            _WComponent.WidgetButton({
+                icon: "del",
+                text: "외원 탈퇴",
+                fonclick: async () => {
+                    if (confirm("정말로 이 계정을 삭제하시겠습니까?\n(이 결정은 번복되지 않습니다.)\n(추가로 다시 한 번 물어보는 절차도 없습니다.)")) {
+                        makeToast("잠시만 기다려 주십시오. 정보가 곧 삭제됩니다.");
+                        await firebase.firestore().collection("user").doc(firebase.auth().currentUser.uid).delete().then(() => makeToast("사용자의 데이터를 모두 삭제하는데 성공하였습니다."));
+                        await firebase.auth().currentUser.delete().then(() => {
+                            alert("사이트에서 당신의 정보를 삭제했습니다.\n(다음에 뵙기를 믿습니다.)");
+                            location.reload();
+                        })
+                        .catch(e => alert(e.code == "auth/requires-recent-login" ? "사용자의 계정을 삭제하는데 실패했습니다.\n사유: 계정 삭제 작업은 중요하므로 최근 인증이 필요합니다.\n재 로그인한 후, 다시 계정 삭제를 진행해주세요." : "알 수 없는 이유로 회원 탈퇴에 실패하였습니다. 다시 한 번 시도해주세요."));
+                    }
+                }
+            })
+        ]
+        if (SDB.value.token) element = element.concat([
+            $("hr"),
+            $("form", {
+                onsubmit: async e => {
+                    e.preventDefault();
+                    dataset.key = e.target[0].value;
+                    DB.value("secret", dataset);
+                    await notifyDataChange();
+                    location.reload();
+                }
+            }).add(
+                $("input", {
+                    type: "text",
+                    style: "background-image: url('/resource/img/icon/lock.png')",
+                    class: "inputWidget",
+                    placeholder: "특수문서 키"
+                })
+            )
+        ])
+        return element;
     }
 }
 
