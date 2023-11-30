f91ab2b2f281722d1d1c5d59490ffae5d7a6f2fa
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2023-11-30 16:41:09 +0900

    Update component.js

--- resource/js/component.js
+++ resource/js/component.js
@@ -37,7 +37,7 @@ const _SComponent = {
     ),
 
     /**
-     * @type {(props: {name: string, exp: string}) => Dom}
+     * @type {(props: {name: string?, exp: string?}) => Dom}
      */
     UserProfile: ({name, exp}) => $("div", {
         class: "userProfile"
