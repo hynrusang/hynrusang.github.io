df338375436f42deae5b42a688d86c6c0fd1c274
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2024-06-05 15:45:14 +0900

    Update Player.js

--- resource/js/page/Player/Player.js
+++ resource/js/page/Player/Player.js
@@ -8,10 +8,11 @@ const Player = new Dynamic.Fragment("player",
     if (playerUrl) {
         let shuffleState = false;
         const parser = playerUrl.match(/list=([^&]+)/);
-        const createShuffleButton = YTPlayer => Dynamic.$("button", {class: "iconX", style: "position: absolute; left: 0px; bottom: 50%; background-image: url(/resource/img/icon/shuffle.png); opacity: 0.5", onclick: e2 => {
+        const createShuffleButton = YTPlayer => Dynamic.$("button", {class: "iconX", style: "position: absolute; left: 0px; bottom: 50%; background-image: url(/resource/img/icon/shuffle.png); opacity: 0.5", onclick: e => {
             YTPlayer.setShuffle(shuffleState = !shuffleState);
+            if (shuffleState) YTPlayer.playVideoAt(0);
             pushSnackbar({message: `셔플 모드를 ${shuffleState ? "" : "비"}활성화 시켰습니다.`, type: "normal"});
-            e2.target.style.opacity = shuffleState ? null : "0.5"
+            e.target.style.opacity = shuffleState ? null : "0.5"
         }})
 
         if (parser) {
@@ -37,9 +38,7 @@ const Player = new Dynamic.Fragment("player",
             new YT.Player("dynamic_player", {
                 videoId: playerUrl.match(/v=([^&]+)/)[1],
                 events: {
-                    'onReady': e => {
-                        e.target.playVideo();
-                    },
+                    'onReady': e => e.target.playVideo(),
                     'onStateChange': e => {
                         if (e.data === YT.PlayerState.ENDED) e.target.playVideo();
                     }
