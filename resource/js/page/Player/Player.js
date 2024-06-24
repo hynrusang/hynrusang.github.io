953e8d7649826ad72ef10a7302c97ec60eaeea83
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2024-06-25 01:06:28 +0900

    Update Player.js

--- resource/js/page/Player/Player.js
+++ resource/js/page/Player/Player.js
@@ -8,29 +8,24 @@ const Player = new Dynamic.Fragment("player",
     let shuffleState = false;
     let targetUrl = url ? (urlTemp = url) : urlTemp;
     const parser = targetUrl.match(/list=([^&]+)/);
-    const createShuffleButton = YTPlayer => Dynamic.$("button", {class: "iconX", style: "position: absolute; left: 0px; bottom: 50%; background-image: url(/resource/img/icon/shuffle.png); opacity: 0.5", onclick: e => {
-        YTPlayer.setShuffle(shuffleState = !shuffleState);
-        pushSnackbar({message: `셔플 모드를 ${shuffleState ? "" : "비"}활성화 시켰습니다.`, type: "normal"});
-        e.target.style.opacity = shuffleState ? null : "0.5"
+    const createShuffleButton = YTPlayer => Dynamic.$("button", {class: "iconX", style: "position: absolute; left: 0px; bottom: 50%; background-image: url(/resource/img/icon/shuffle.png);", onclick: e => {
+        YTPlayer.setShuffle(true);
+        YTPlayer.playVideoAt(0);
+        pushSnackbar({message: "재생목록을 섞었습니다.", type: "normal"});
     }})
     new YT.Player("dynamic_player", {
         playerVars: parser ? {
             listType: 'playlist',
             list: parser[1],
             loop: 1
-        } : null,
-        loop: 1,
+        } : { 
+            loop: 1 
+        },
         videoId: !parser ? targetUrl.match(/v=([^&]+)/)[1] : null,
         events: {
             'onReady': e => {
                 e.target.playVideo();
                 if (parser) Dynamic.snipe("fragment[rid=player]").add(createShuffleButton(e.target));
-            },
-            'onStateChange': e => {
-                if (e.data === YT.PlayerState.ENDED) {
-                    if (parser && shuffleState && e.target.getPlaylistIndex() === e.target.getPlaylist().length - 1) e.target.setShuffle(true);
-                }
-                console.log(e)
             },
             'onError': e => {
                 pushSnackbar({message: parser ? "재생할 수 없는 동영상을 건너뛰었습니다." : "재생할 수 없는 동영상입니다.", type: "error"});
