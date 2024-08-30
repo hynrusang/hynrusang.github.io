d715c2774c085218aa1f3ce51bb1b44a1de68f6f
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2024-08-30 15:11:10 +0900

    Update application.js

--- resource/js/init/application.js
+++ resource/js/init/application.js
@@ -1,13 +1,13 @@
 import { Dynamic } from "./module.js";
 import DataResource from "../util/DataResource.js";
 import Login from "../page/Login.js";
-import Memo from "../page/Note/Memo.js";
 import Playlist from "../page/Player/Playlist.js";
+import Link from "../page/Note/Link.js";
 
 DataResource.init();
 DataResource.Data.navigator.note = {
     label: "텍스트 데이터 관련",
-    page: Memo
+    page: Link
 }
 DataResource.Data.navigator.player = {
     label: "유튜브 플레이어 관련",
