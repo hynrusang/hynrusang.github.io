a747ac4fd3e623e32aa5d46da531ce96c3910db5
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2025-07-13 22:59:56 +0900

    4.13
    유튜브 내부 API 호출방식 변경

--- resource/js/page/Router.js
+++ resource/js/page/Router.js
@@ -2,17 +2,10 @@ import { Dynamic } from "../init/module.js";
 import { IconX } from "../component/XBox.js";
 import Memo from "./Note/Memo.js";
 import Link from "./Note/Link.js";
-import Playlist from "./Player/Playlist.js";
-import Player from "./Player/Player.js";
 
 const MainRouter = [
     IconX({icon: "link", onclick: () => Dynamic.FragMutation.mutate(Link)}),
     IconX({icon: "memo", onclick: () => Dynamic.FragMutation.mutate(Memo)})
 ]
 
-const PlayerRouter = [
-    IconX({icon: "database", onclick: () => Dynamic.FragMutation.mutate(Playlist)}),
-    IconX({icon: "https://www.youtube.com/", onclick: () => Dynamic.FragMutation.mutate(Player)})
-]
-
-export { MainRouter, PlayerRouter };
+export { MainRouter  };
