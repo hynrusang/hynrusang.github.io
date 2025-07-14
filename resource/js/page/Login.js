792ef1c4d2c4033406323327b78159c05034b309
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2025-07-14 15:11:46 +0900

    Update Login.js

--- resource/js/page/Login.js
+++ resource/js/page/Login.js
@@ -1,7 +1,7 @@
 import { Dynamic } from "../init/module.js";
 import { LoginForm } from "../component/FormBox.js";
 
-const Login = new Dynamic.Fragment("setting", 
+const Login = new Dynamic.Fragment("rander", 
     Dynamic.$("div", {class: "screenX", style: "padding-top: 15vh;"}).add(
         Dynamic.$("h1", {text: "WELCOME TO HBOX"}),
         Dynamic.$("p", {text: "이메일과 비밀번호를 입력해주세요."}),
