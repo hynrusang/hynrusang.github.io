192497a12ea9e41adafe9ff2888d771b249d016d
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2025-07-14 15:57:44 +0900

    up

--- resource/js/page/Player.js
+++ resource/js/page/Player.js
@@ -194,8 +194,8 @@ const Player = new Dynamic.Fragment("player",
         }}).add(
             Dynamic.$("img", { src: "https://yt3.ggpht.com/2eI1TjX447QZFDe6R32K0V2mjbVMKT5mIfQR-wK5bAsxttS_7qzUDS1ojoSKeSP0NuWd6sl7qQ=s88-c-k-c0x00ffffff-no-rj" }),
             Dynamic.$("span", { class: "playlist-title-label" }).add(
-                Dynamic.$("i", { class: "ytv-arrow down", text: "▲" }),
-                TitleLabel
+                TitleLabel,
+                Dynamic.$("div", { class: "ytv-arrow-triangle" })
             )
         )
     )
