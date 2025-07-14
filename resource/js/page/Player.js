eb14b7b6e03f72bf5bd16e6b1f9d5c47edb62223
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2025-07-14 22:22:07 +0900

    up

--- resource/js/page/Player.js
+++ resource/js/page/Player.js
@@ -198,7 +198,7 @@ const Player = new Dynamic.Fragment("player",
             Dynamic.$("img", { src: "https://yt3.ggpht.com/2eI1TjX447QZFDe6R32K0V2mjbVMKT5mIfQR-wK5bAsxttS_7qzUDS1ojoSKeSP0NuWd6sl7qQ=s88-c-k-c0x00ffffff-no-rj" }),
             Dynamic.$("span", { class: "playlist-title-label" }).add(
                 TitleLabel,
-                Dynamic.$("div", { class: "ytv-arrow-triangle" })
+                Dynamic.$("div", { class: "ytv-arrow-triangle", text: "▼" })
             )
         )
     )
