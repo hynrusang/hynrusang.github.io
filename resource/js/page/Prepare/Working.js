ad56a7bc9ecf3b31e01e3522649e10c8bb675970
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2025-08-04 18:54:32 +0900

    Update Working.js

--- resource/js/page/Prepare/Working.js
+++ resource/js/page/Prepare/Working.js
@@ -1,12 +1,12 @@
 import { Dynamic } from "../../init/module.js";
 
-const message = Dynamic.$("h1", { id: "loading_text" });
+const messageText = Dynamic.$("h1", { id: "loading_text" });
 
 const Randering = new Dynamic.Fragment("rander", 
     Dynamic.$("div", {style: "display: flex; flex-direction: column; height: 100%; justify-content: center; align-items: center;"}).add(
         Dynamic.$("div", {id: "loading_icon"}),
-        message
+        messageText
     )
-).registAction(message => message.innerText = message);
+).registAction(message => messageText.innerText = message);
 
 export default Randering;
