0164173e4ee9dbafa708a1ee2af4f7d6734b999a
환류상 (가명) <84229112+hynrusang@users.noreply.github.com>
2026-04-13 08:30:04 +0900

    Increase window size for player from 6 to 10

--- resource/js/page/Player.js
+++ resource/js/page/Player.js
@@ -563,7 +563,7 @@ class PlayerService {
     #uiManager;
 
     // 전체 큐 대신 작은 윈도우만 플레이어에 탑재
-    #windowSize = 6;
+    #windowSize = 10;
     #windowAbsIndices = [];
     #windowReloadLock = false;
     #windowReloadTimer = null;
